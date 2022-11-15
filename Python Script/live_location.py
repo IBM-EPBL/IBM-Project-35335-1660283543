f7bcf4ed0293c4d25e25b60af7eb2b18bc80ebe7
JeevaYovan <109798509+JeevaYovan@users.noreply.github.com>
2022-11-15 21:20:43 +0530

    Update live_location.py

--- Python Script/live_location.py
+++ Python Script/live_location.py
@@ -1,1 +1,43 @@
+import wiotp.sdk.device
+import time
+import random
+myConfig = {
+  "identity": {
+    "orgId": "nybqe1",
+    "typeId": "Jeeva_Yovan",
+    "deviceId": "Watson"
+  },
+  "auth": {
+    "token": "12345678"
+  }
+}
 
+def myCommandCallback(cmd):
+  print("Message received from IBM IoT Platform: %s" % cmd.data['command'])
+  m=cmd.data['command']
+
+def pub(data):
+  client.publishEvent(eventId="status", msgFormat="json", data=myData, qos=0, onPublish=None)
+  print("Published data Successfully:%s",myData)
+
+client = wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
+client.connect()
+
+while True:
+  myData={'name':'Train1','lat':10.184363,'lon': 77.922702}
+  pub(myData)
+  time.sleep(3)
+  myData={'name':'Train1','lat':10.213225,'lon': 77.898765}
+  pub(myData)
+  time.sleep(3)
+  myData={'name':'Train1','lat':10.285035,'lon': 77.921569}
+  pub(myData)
+  time.sleep(3)
+  myData={'name':'Train1','lat':10.343369,'lon': 77.958056}
+  pub(myData)
+  time.sleep(3)
+  myData={'name':'Train1','lat':10.356829,'lon': 77.980861}
+  pub(myData)
+  time.sleep(3)
+  client.commandCallback = myCommandCallback
+client.disconnect()
